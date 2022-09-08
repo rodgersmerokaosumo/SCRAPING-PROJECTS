f18aa2992659387dc9fa015ef6773de839e92d28
osumorodgersmeroka <osumorodgersmeroka@gmail.com>
2022-09-08 19:03:18 +0300

    mitv: cleaned channel names b4 search

--- justwatch/justwatch.py
+++
@@ -1,6 +0,0 @@
-from requests_html import  HTML, HTMLSession
-session = HTMLSession()
-url = "https://www.justwatch.com/us/movies?genres=act&release_year_from=2022"
-r = session.get(url)
-r = r.html.render(scrolldown=5)
-print(r.html.find("div.title-list-grid__item"))
